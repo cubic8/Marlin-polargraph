f4069e74e6dd2bd400373f8cf804b86c0c90b5ee
Steve <steve@cubic8.com>
2022-08-26 10:13:17 +1000

    Hack apply_motion_limits() so target is not multiplied by zero

--- Marlin/src/module/motion.cpp
+++ Marlin/src/module/motion.cpp
@@ -854,7 +854,7 @@ void restore_feedrate_and_scaling() {
         constexpr xy_pos_t offs{0};
       #endif
 
-      if (TERN1(IS_SCARA, axis_was_homed(X_AXIS) && axis_was_homed(Y_AXIS))) {
+      if (0) { //TERN1(IS_SCARA, axis_was_homed(X_AXIS) && axis_was_homed(Y_AXIS))) {
         const float dist_2 = HYPOT2(target.x - offs.x, target.y - offs.y);
         if (dist_2 > delta_max_radius_2)
           target *= float(delta_max_radius / SQRT(dist_2)); // 200 / 300 = 0.66
